1c9413d570359e9a46a872182424e473b565665d
Nanditha Shivaram <nanditha.shivaram2@transport.nsw.gov.au>
2021-11-29 05:24:22 +0000

    first commit

---
+++ webapp/controller/Object.controller.js
@@ -0,0 +1,172 @@
+/*global location*/
+sap.ui.define([
+	"au/com/bournedigital/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"au/com/bournedigital/model/formatter"
+], function (
+	BaseController,
+	JSONModel,
+	History,
+	formatter
+) {
+	"use strict";
+
+	return BaseController.extend("au.com.bournedigital.controller.Object", {
+
+		formatter: formatter,
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/**
+		 * Called when the worklist controller is instantiated.
+		 * @public
+		 */
+		onInit: function () {
+			// Model used to manipulate control states. The chosen values make sure,
+			// detail page is busy indication immediately so there is no break in
+			// between the busy indication for loading the view's meta data
+			var iOriginalBusyDelay,
+				oViewModel = new JSONModel({
+					busy: true,
+					delay: 0
+				});
+
+			var oTotalValueModel = new JSONModel({
+				totalValue: 0
+			});
+			this.getView().setModel(oTotalValueModel, "totalValueModel");
+			sap.ui.getCore().setModel(oTotalValueModel, "totalValueModel"); 
+
+			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+
+			// Store original busy indicator delay, so it can be restored later on
+			iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+			this.setModel(oViewModel, "objectView");
+			this.getOwnerComponent().getModel().metadataLoaded().then(function () {
+				// Restore original busy indicator delay for the object view
+				oViewModel.setProperty("/delay", iOriginalBusyDelay);
+			});
+			//this.byId("totalValueText").setText(sap.ui.getCore().getModel("totalValueModel").getProperty("/totalValue"));
+		},
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		/**
+		 * Event handler when the share in JAM button has been clicked
+		 * @public
+		 */
+		onShareInJamPress: function () {
+			var oViewModel = this.getModel("objectView"),
+				oShareDialog = sap.ui.getCore().createComponent({
+					name: "sap.collaboration.components.fiori.sharing.dialog",
+					settings: {
+						object: {
+							id: location.href,
+							share: oViewModel.getProperty("/shareOnJamTitle")
+						}
+					}
+				});
+			oShareDialog.open();
+		},
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+		/**
+		 * Binds the view to the object path.
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+		_onObjectMatched: function (oEvent) {
+			var sObjectId = oEvent.getParameter("arguments").objectId;
+
+			this.getModel().metadataLoaded().then(function () {
+				var sObjectPath = this.getModel().createKey("Orders", {
+					OrderID: sObjectId
+				});
+				this._bindView("/" + sObjectPath);
+/*				var oOjectModel = new JSONModel();
+				oOjectModel.data = */
+				this.getOwnerComponent(this).getModel().read("/" + sObjectPath + "/Order_Details", {
+					success: function (oResponse) {
+						var totalValue = 0;
+						//Calculate totalValue
+						for (var i = 0; i < oResponse.results.length; i++) {
+							totalValue = totalValue + parseInt(oResponse.results[i].UnitPrice);
+						}
+						sap.ui.getCore().getModel("totalValueModel").setProperty("/totalValue", totalValue);
+					},
+					error: function (oResponse) {
+
+					}
+				});
+				//this.setModel(oOjectModel, "objectModel");
+			}.bind(this));
+		},
+
+		/**
+		 * Binds the view to the object path.
+		 * @function
+		 * @param {string} sObjectPath path to the object to be bound
+		 * @private
+		 */
+		_bindView: function (sObjectPath) {
+			var oViewModel = this.getModel("objectView"),
+				oDataModel = this.getModel();
+
+			this.getView().bindElement({
+				path: sObjectPath,
+				events: {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function () {
+						oDataModel.metadataLoaded().then(function () {
+							// Busy indicator on view should only be set if metadata is loaded,
+							// otherwise there may be two busy indications next to each other on the
+							// screen. This happens because route matched handler already calls '_bindView'
+							// while metadata is loaded.
+							oViewModel.setProperty("/busy", true);
+						});
+					},
+					dataReceived: function () {
+						oViewModel.setProperty("/busy", false);
+					}
+				}
+			});
+		},
+
+		_onBindingChange: function () {
+			var oView = this.getView(),
+				oViewModel = this.getModel("objectView"),
+				oElementBinding = oView.getElementBinding();
+
+			// No data for the binding
+			if (!oElementBinding.getBoundContext()) {
+				this.getRouter().getTargets().display("objectNotFound");
+				return;
+			}
+
+			var oResourceBundle = this.getResourceBundle(),
+				oObject = oView.getBindingContext().getObject(),
+				sObjectId = oObject.OrderID,
+				sObjectName = oObject.CustomerID;
+
+			// Everything went fine.
+			oViewModel.setProperty("/busy", false);
+			oViewModel.setProperty("/saveAsTileTitle", oResourceBundle.getText("saveAsTileTitle", [sObjectName]));
+			oViewModel.setProperty("/shareOnJamTitle", sObjectName);
+			oViewModel.setProperty("/shareSendEmailSubject",
+				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+			oViewModel.setProperty("/shareSendEmailMessage",
+				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+		}
+
+	});
+
+});
